981989c1e224e7c5bd4a3cc246df443515ca60c2
olivera-jesus <99697989+olivera-jesus@users.noreply.github.com>
2022-03-18 19:31:01 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace lecturadearchivo
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+
+        {
+            string line;
+            try
+            {
+                StreamReader sr = new StreamReader("C:\\k020xd.txt");
+                line = sr.ReadLine();
+                while( line != null)
+                {
+                    Console.WriteLine(line);
+                    line = sr.ReadLine();
+
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+             catch(Exception e)
+            {
+                Console.WriteLine("excepcion : " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("ejecutando bloque final");
+            }
+        }
+    }
+}
